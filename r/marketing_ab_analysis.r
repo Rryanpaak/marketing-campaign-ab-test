44dd0890873507a7c283f2c60215f5ce5d8af533
Ryan <park784612@gmail.com>
2026-09-14 15:46:40 +0900

    Fix R proportion test syntax

--- r/marketing_ab_analysis.r
+++ r/marketing_ab_analysis.r
@@ -1,5 +1,5 @@
 # A/B test statistical analysis for marketing campaign
-# Purpose: To compare the conversion rates between two different group; Ad and PSA
+# Purpose: To compare the conversion rates between two groups: Ad and PSA
 
 # Load the data
 summary_ab <- read.csv(file.choose())
@@ -14,11 +14,12 @@ ad_converted <- summary_ab$converted_user[summary_ab$test_group == "ad"]
 psa_total <- summary_ab$total_user[summary_ab$test_group == "psa"]
 psa_converted <- summary_ab$converted_user[summary_ab$test_group == "psa"]
 
-# Prop.test
-prop.test(
-    x <- c(ad_converted, psa_converted),
-    n <- c(ad_total, psa_total),
+# Proportion test
+ab_test <- prop.test(
+    x = c(ad_converted, psa_converted),
+    n = c(ad_total, psa_total),
     alternative = "greater",
-    correct = false
+    correct = FALSE
 )
 
+ab_test
